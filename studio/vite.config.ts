ee1ddc2fd2cb3d70c07a94764717891beea056d4
Ahmad Faiz <faiz@faizdb.io>
2026-09-01 10:59:43 +0800

    chore: Change Studio default port to 27020 to prevent local port conflicts

--- studio/vite.config.ts
+++ studio/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 27020, // Dedicated FaizDB Studio Port
     host: '0.0.0.0',
     proxy: {
       '/v1': {
